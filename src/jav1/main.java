1585288132f327e8a317ebc4ef4a915863be2991
Danielle Mbenda <danimbenda@gmail.com>
2014-08-20 21:30:50 -0500

    'gothen'

---
+++ src/jav1/main.java
@@ -0,0 +1,21 @@
+package jav1;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.print("this is an hello world \n");
+		// TODO Auto-generated method stub
+		params brice = new params();
+		brice.name = "brice donald ngnigha";
+		brice.age = 29;
+		brice.gender = true;
+		System.out.print("Name: " + brice.name + "\nAge = " + brice.age + 
+						 " \nMale?: " + brice.gender);
+	}
+
+}
---
+++ src/jav1/params.java
@@ -0,0 +1,13 @@
+package jav1;
+
+public class params 
+{
+	String name;
+	int age;
+	boolean gender; // one for male, 0 for female
+
+	public params() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
